9d02d58df33a6a2d43b9324d678843f376d0062d
Your Name <you@example.com>
2026-03-31 22:35:49 +0530

    updated code at git 31 10:35

--- src/routes/showtasks.js
+++ src/routes/showtasks.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
+// const taskCrudCtrl = require("../controller/task_crud");
 const taskCrudCtrl = require("../controller/task_crud");
-
 // router.get("/", (req, res) => {
 //     res.send("Api working perfectly");
 // });
 router.get("/", taskCrudCtrl.showTask);
-router.get("/:id", taskCrudCtrl.findTaskById);
 router.post("/addTask", taskCrudCtrl.addTask);
 router.get("/searchTask", taskCrudCtrl.searchTask);
 router.get("/statusFilter", taskCrudCtrl.filterBasedOnStatus);
+router.get("/:id", taskCrudCtrl.findTaskById);
 router.patch("/update/:id", taskCrudCtrl.updateTask);
 router.delete("/delete/:id", taskCrudCtrl.deleteTask);
 
